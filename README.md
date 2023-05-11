aa3e5dfdc1c936cdb602519a04aba1cc8e233a55
Matti <mfinder@campus.tu-berlin.de>
2023-05-11 20:49:30 +0200

    wrote Readme semi working

--- README.md
+++ README.md
@@ -8,19 +8,55 @@
 
 Erwartet ein json Objekt mit {"deadline": "<date>", "description": "<desc>", "percent_done": <pd>}.
 
-Wobei <date> ein Datum als String im Format yyyy-mm-dd ist, <desc> ein String mit unter 100 chars ist und <pd> eine Zahl zwischen 0 und 100.
+```
+{
+  "deadline": "2023-03-19",
+  "description": "hallo bitte einmal auf das hier ändern",
+  "percent_done": 20
+}
+```
+
+Wobei <date> ein Datum als String im Format yyyy-mm-dd ist, <desc> ein String mit unter 160 chars ist und <pd> eine Zahl zwischen 0 und 100.
+Es muss hierbei der Content-Type im Http Header auf application/json gesetzt werden.
 
 #### GET
 
-Alle Todos in eine json objekt wie oben beschrieben.
+Alle Todos in eine json Objekt mit Key todos und dann einer Liste mit Daten im Format von oben.
+
+```
+{
+  "todos": [
+    {
+      "id": 2,
+      "deadline": "2023-03-19",
+      "description": "hallo",
+      "percent_done": 20
+    },
+    {
+      "id": 3,
+      "deadline": "2020-18-19",
+      "description": "Ich bin eine Beschreibung",
+      "percent_done": 10
+    }
+  ]
+}
+```
 
 
 ### /api/todo/<todo_id>
 
 #### DELETE
 
-löscht das entsprechende Todo.
+Löscht das entsprechende Todo.
 
 #### POST
 
-Updatet die im json übergebenen Werte im entsprechenden Todo.
+Updatet die im json übergebenen Werte im entsprechenden Todo, es reicht eine Teilmenge und nur diese werden dann geupdatet.
+
+```
+{
+  "percent_done": 30
+}
+```
+
+Es muss hierbei der Content-Type im Http Header auf application/json gesetzt werden.
--- app.py
+++ app.py
@@ -6,14 +6,32 @@ from mysql.connector import errorcode
 app = Flask('api')
 
 get_todos_query = "SELECT * FROM test_todos"
+
 delete_todos_query = "DELETE FROM test_todos where id = %s"
+
 insert_todo_query = "INSERT INTO test_todos (deadline, description, percent_done) VALUES (%s, %s, %s)"
-create_todo_table = "create table test_todos(deadline varchar(10), description varchar(100), percent_done INTEGER)"
+
+update_desc_query= "UPDATE test_todos " \
+                   "SET description = %s " \
+                   "WHERE id = %s"
+update_pd_query= "UPDATE test_todos " \
+                   "SET percent_done = %s " \
+                   "WHERE id = %s"
+update_date_query= "UPDATE test_todos " \
+                   "SET deadline = %s " \
+                   "WHERE id = %s"
+
+create_todo_table = "CREATE TABLE IF NOT EXISTS test_todos(" \
+                    "id MEDIUMINT NOT NULL AUTO_INCREMENT, " \
+                    "deadline VARCHAR(10) NOT NULL , " \
+                    "description VARCHAR(160) NOT NULL , " \
+                    "percent_done INTEGER NOT NULL," \
+                    "PRIMARY KEY (id))"
 
 
 def connect_to_db():
     try:
-        cnc = mysql.connector.connect(user='web_user', password='web', host='127.0.0.1', database='todo_db')
+        cnc = mysql.connector.connect(user='web_user', password='supersecureweb', host='192.168.178.85', database='todo_db')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -30,11 +48,13 @@ def list_programming_languages():
     db_cnc = connect_to_db()
     cursor = db_cnc.cursor()
     cursor.execute(get_todos_query)
-    todos = cursor.fetchall()
-    #TODO: parse output and fill a json object with it
+    todos_list = cursor.fetchall()
     db_cnc.close()
-    dump = json.dumps(todos)
-    return dump
+    todos = []
+    for id, dl, desc, pd in todos_list:
+        todos.append(dict(id = id, deadline = dl, description = desc, percent_done = pd))
+    todos_json = json.dumps(dict(todos = todos))
+    return todos_json
 
 
 @app.post('/api/todo')
@@ -49,15 +69,38 @@ def put_new_todo():
     return {"exit": "success"}
 
 
+@app.post('/api/todo/<todo_id>')
+def update_todo(todo_id):
+    vals_to_update = request.json
+    db_cnc = connect_to_db()
+    cursor = db_cnc.cursor()
+
+    if 'description' in vals_to_update:
+        cursor.execute(update_desc_query, (vals_to_update['description'], todo_id))
+    if 'percent_done' in vals_to_update:
+        cursor.execute(update_pd_query, (vals_to_update['percent_done'], todo_id))
+    if 'deadline' in vals_to_update:
+        cursor.execute(update_date_query, (vals_to_update['deadline'], todo_id))
+
+    db_cnc.commit()
+    db_cnc.close()
+    return {"exit": "success"}
+
+
 @app.delete('/api/todo/<todo_id>')
 def delete_todo(todo_id):
     db_cnc = connect_to_db()
     cursor = db_cnc.cursor()
-    cursor.execute(get_todos_query, (todo_id,))
+    cursor.execute(delete_todos_query, (todo_id,))
     db_cnc.commit()
     db_cnc.close()
-    pass
+    return {"exit": "success"}
 
 
 if __name__ == '__main__':
-    list_programming_languages()
+    db_cnc = connect_to_db()
+    cursor = db_cnc.cursor()
+    cursor.execute(create_todo_table)
+    db_cnc.commit()
+    db_cnc.close()
+    print(list_programming_languages())
